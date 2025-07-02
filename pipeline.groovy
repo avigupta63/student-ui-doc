36ce2d8847799307ebbd265c9daa5fb87780eb10
avigupta63 <avi.gupta63@gmail.com>
2025-07-02 04:26:04 +0000

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -14,7 +14,7 @@ pipeline {
             }
             stage ('test') {
                 steps {
-                    sh '''/opt/maven/bin/mvn clean verify sonar:sonar \\
+                    sh '''/opt/maven/bin/mvn sonar:sonar \\
   -Dsonar.projectKey=studentapp \\
   -Dsonar.host.url=http://18.226.104.205:9000 \\
   -Dsonar.login=dc92b1d4165982f6fd7d9c260e643594e0fa7a0f'''
@@ -22,7 +22,7 @@ pipeline {
             }
             stage ('deploy') {
                 steps {
-                    echo 'deploy success'
+                    deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: '79fb3a07-3783-4419-86b6-3a79e9fb7a82', path: '', url: 'http://18.217.227.244:8080/')], contextPath: null, war: '**/*.war'
                 }
             }
         }
