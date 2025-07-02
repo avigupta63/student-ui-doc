af779c8b89469ce9a1febc1de3304bf4b47f60d1
avigupta63 <avi.gupta63@gmail.com>
2025-07-02 11:33:08 +0000

    pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,6 +20,13 @@ pipeline {
                     }
                 }
             }
+            stage ('sonar_quality_gate') {
+                steps {
+                    timeout(10) {
+                    }
+                    waitForQualityGate true
+                    }
+            }
             stage ('deploy') {
                 steps {
                     deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: '79fb3a07-3783-4419-86b6-3a79e9fb7a82', path: '', url: 'http://13.59.38.113:8080/')], contextPath: 'studentapp', war: '**/*.war'
